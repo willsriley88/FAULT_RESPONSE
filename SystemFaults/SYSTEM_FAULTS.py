49b012e8b6318369df3f535a14a80323a552def9
willsriley <willsriley88@gmail.com>
2023-12-20 21:19:30 +1100

    initial commit

---
+++ SystemFaults/SYSTEM_FAULTS.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+
+class DBFaultResponse:
+    response = {
+        "CELL_NAME": None,
+        "SYSTEM_NAME": None,
+        "FAULT": None
+    }
+
+
+class SystemFaults:
+    def __init__(self, system_name="", cell_name="", fault_url=""):
+        self.system_name = system_name
+        self.cell_name = cell_name
+        self.fault_url = fault_url
+        self.fault_response = DBFaultResponse()
+        self.fault_response.response["CELL_NAME"] = self.cell_name
+        self.fault_response.response["SYSTEM_NAME"] = self.system_name
+
+    def update_response(self, fault):
+        try:
+            self.fault_response.response["FAULT"] = fault
+            requests.post(url=self.fault_url, json=self.fault_response.response)
+        except Exception as e:
+            return None
